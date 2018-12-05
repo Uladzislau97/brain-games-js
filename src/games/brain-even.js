a3f7d214783da5e7aa16af108127d47b35fac000
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2018-12-05 18:49:40 +0300

    fix(game): fix brain-even module's random numbers range

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,11 +1,12 @@
 import runGame from './game-engine';
 
-const MIN_RANDOM_NUMBER = 1;
-const MAX_RANDOM_NUMBER = 100;
+const INCLUDED_MIN_RANDOM_NUMBER = 1;
+const EXCLUDED_MAX_RANDOM_NUMBER = 101;
 const GAME_DESCRIPTION = 'Answer "yes" if number even otherwise answer "no".';
 
 const generateQuestion = () => Math.floor(
-  Math.random() * (MAX_RANDOM_NUMBER - MIN_RANDOM_NUMBER) + MIN_RANDOM_NUMBER,
+  Math.random() * (EXCLUDED_MAX_RANDOM_NUMBER - INCLUDED_MIN_RANDOM_NUMBER)
+    + INCLUDED_MIN_RANDOM_NUMBER,
 );
 
 const getCorrectAnswer = (question) => {
