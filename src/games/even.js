c319be5a99d0e8bdf47df290c4941f2331cd6afa
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2018-12-06 16:50:35 +0300

    refactor(game): change 'isEven' method responsibility

--- src/games/even.js
+++ src/games/even.js
@@ -3,14 +3,11 @@ import generateRandomNumber from '../utils';
 
 const gameDescription = 'Answer "yes" if number even otherwise answer "no".';
 
-const isEven = (number) => {
-  if (number % 2 === 0) return 'yes';
-  return 'no';
-};
+const isEven = number => number % 2 === 0;
 
 const generateTask = () => {
   const question = generateRandomNumber();
-  const correctAnswer = isEven(question);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
   return { question, correctAnswer };
 };
 
