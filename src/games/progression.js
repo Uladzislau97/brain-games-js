154864bb8e5d7d8c5c0eb7dc7bbbb1ea2c978b4c
Uladzislau Vasilyeu <VSvasilyev97@gmail.com>
2018-12-06 14:02:21 +0300

    feat(game): add brain-progression game

---
+++ src/games/progression.js
@@ -0,0 +1,35 @@
+import runGame from '../game-engine';
+import generateRandomNumber from '../utils';
+
+const gameDescription = 'What number is missing in the progression?';
+
+const progressionLength = 10;
+const includedMinRandomIndex = 0;
+const excludedMaxRandomIndex = 10;
+const includedMinRandomCommonDifference = 1;
+const excludedMaxRandomCommonDifference = 11;
+
+const generateSequence = (firstElement, commonDifference) => Array(progressionLength)
+  .fill(firstElement).map((element, index) => element + commonDifference * index);
+
+const generateSequenceWithHiddenElement = (sequence, hiddenElementIndex) => [
+  ...sequence.slice(0, hiddenElementIndex), '..', ...sequence.slice(hiddenElementIndex + 1),
+];
+
+const generateTask = () => {
+  const firstElement = generateRandomNumber();
+  const commonDifference = generateRandomNumber(
+    includedMinRandomCommonDifference, excludedMaxRandomCommonDifference,
+  );
+  const sequence = generateSequence(firstElement, commonDifference);
+  const hiddenElementIndex = generateRandomNumber(
+    includedMinRandomIndex, excludedMaxRandomIndex,
+  );
+  const correctAnswer = sequence[hiddenElementIndex].toString();
+  const question = generateSequenceWithHiddenElement(sequence, hiddenElementIndex).join(' ');
+  return { question, correctAnswer };
+};
+
+export default () => {
+  runGame(gameDescription, generateTask);
+};
